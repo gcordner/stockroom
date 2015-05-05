5460f3edd15aa2771116030e17c75b646275a11c
Vishwas Bhatnagar <vishwas.bhatnagar@a3logics.in>
2015-05-05 16:46:22 +0530

    associating sets with bundled product (Fixes #93602214)

--- app/code/local/Gec/Customimport/Block/Customimport.php
+++ app/code/local/Gec/Customimport/Block/Customimport.php
@@ -715,8 +715,8 @@ class Gec_Customimport_Block_Customimport extends Gec_Customimport_Block_Catalog
         $product->setSku((string)$item->id); //Product custom id
         $product->setWebsiteIds(array(Mage::app()->getStore(true)->getWebsite()->getId()));
         $product->setStoreIDs(array($this->_store_id));    // Default store id .
-
-        $product->setAttributeSetId($asid);
+        $attid = $this->_getMageId($asid);
+        $product->setAttributeSetId($attid);
         $product->setData('name', (string)$item->name);
         $product->setPrice((real)$item->price);
         $product->setWeight((real)$item->weight);
