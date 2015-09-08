37b0d8fb72450840caab987e5ef27e722e003bc8
Intelliant01 <intelliant01@intelliant.net>
2015-09-08 03:32:41 -0700

    Installed Mirasvit FPC plugin [#93532908]

---
+++ app/code/local/Mirasvit/Fpc/Helper/Code.php
@@ -0,0 +1,10 @@
+<?php
+class Mirasvit_Fpc_Helper_Code extends Mirasvit_MstCore_Helper_Code
+{
+    protected $k = "5PECQARLTX";
+    protected $s = "FPC";
+    protected $l = "12927";
+    protected $v = "1.0.1";
+    protected $b = "394";
+    protected $d = "ec2-54-244-250-139.us-west-2.compute.amazonaws.com";
+}
