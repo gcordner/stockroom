f5b9187e05fc056558b0b63c39232ec40b98426c
gunjanr <gunjan@krishinc.com>
2015-09-17 12:06:48 +0530

    salecheck FPC clean code fix. [#103401034]

--- salecheck.php
+++ salecheck.php
@@ -1,23 +1,23 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
- 
-// Load Up Magento Core
-define('MAGENTO', realpath(''));
-
-require_once(MAGENTO . '/app/Mage.php');
- 
+// Change current directory to the directory of current script
+chdir(dirname(__FILE__));
+require 'app/Mage.php';
 $app = Mage::app();
 $curtime = strtotime(Mage::getModel('core/date')->date('Y-m-d H:i:s'));
+//echo date('Y-m-d H:i:s',$curtime).'<br />';
 $todaystart =  date('Y-m-d H:i:s', mktime(date('H',$curtime),date('i',$curtime),'0', date('m',$curtime),date('d',$curtime),date('Y',$curtime)));
 $todaynext =  date('Y-m-d H:i:s', mktime(date('H',$curtime),date('i',$curtime),'59', date('m',$curtime),date('d',$curtime),date('Y',$curtime)));
 $collection = Mage::getModel('catalog/product')->getCollection()
                 ->addAttributeToSelect(array('special_from_date', 'special_to_date'))
                 ->addAttributeToFilter( 'special_from_date' , array( 'gteq' => $todaystart ))
                 ->addAttributeToFilter( 'special_from_date' , array( 'lteq' => $todaynext ));
-                echo $collection->getSelect();
+                //echo '<br />'.$collection->getSelect();
 foreach($collection as $product){
-    Mage::app()->getCache()->clean('matchingAnyTag', 'CATALOG_PRODUCT_'.$product->getId());
+    //Mage::app()->getCache()->clean('matchingAnyTag', 'CATALOG_PRODUCT_'.$product->getId());
+    //Mage::app()->getCache()->clean();
+    Mirasvit_Fpc_Model_Cache::getCacheInstance()->clean('CATALOG_PRODUCT_'.$product->getId());
 }
 
 $collection = Mage::getModel('catalog/product')->getCollection()
@@ -26,7 +26,8 @@ $collection = Mage::getModel('catalog/product')->getCollection()
                 ->addAttributeToFilter( 'special_to_date' , array( 'lteq' => $todaynext ));
                 echo '<br />'.$collection->getSelect();
 foreach($collection as $product){
-    Mage::app()->getCache()->clean('matchingAnyTag', 'CATALOG_PRODUCT_'.$product->getId());
+    //Mage::app()->getCache()->clean(array('FPC'));
+    Mirasvit_Fpc_Model_Cache::getCacheInstance()->clean('CATALOG_PRODUCT_'.$product->getId());
 }
 
 ?>
