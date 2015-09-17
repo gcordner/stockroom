282bdffdf9ceea22ee56d6f24728961a8fc2bc44
Intelliant01 <intelliant01@intelliant.net>
2015-09-17 09:25:33 +0000

    Refactored product sale checking and cache clearing script [#103401034]

--- salecheck.php
+++ salecheck.php
@@ -1,22 +1,20 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+
 // Change current directory to the directory of current script
 chdir(dirname(__FILE__));
 require 'app/Mage.php';
+
 $app = Mage::app();
 $curtime = strtotime(Mage::getModel('core/date')->date('Y-m-d H:i:s'));
-//echo date('Y-m-d H:i:s',$curtime).'<br />';
+//echo "\n".date('Y-m-d H:i:s',$curtime); // uncomment for debugging
 $todaystart =  date('Y-m-d H:i:s', mktime(date('H',$curtime),date('i',$curtime),'0', date('m',$curtime),date('d',$curtime),date('Y',$curtime)));
 $todaynext =  date('Y-m-d H:i:s', mktime(date('H',$curtime),date('i',$curtime),'59', date('m',$curtime),date('d',$curtime),date('Y',$curtime)));
 $collection = Mage::getModel('catalog/product')->getCollection()
                 ->addAttributeToSelect(array('special_from_date', 'special_to_date'))
                 ->addAttributeToFilter( 'special_from_date' , array( 'gteq' => $todaystart ))
                 ->addAttributeToFilter( 'special_from_date' , array( 'lteq' => $todaynext ));
-                //echo '<br />'.$collection->getSelect();
+//                echo "\n".$collection->getSelect(); // uncomment for debugging
 foreach($collection as $product){
-    //Mage::app()->getCache()->clean('matchingAnyTag', 'CATALOG_PRODUCT_'.$product->getId());
-    //Mage::app()->getCache()->clean();
     Mirasvit_Fpc_Model_Cache::getCacheInstance()->clean('CATALOG_PRODUCT_'.$product->getId());
 }
 
@@ -24,9 +22,8 @@ $collection = Mage::getModel('catalog/product')->getCollection()
                 ->addAttributeToSelect(array('special_from_date', 'special_to_date'))
                 ->addAttributeToFilter( 'special_to_date' , array( 'gteq' => $todaystart ))
                 ->addAttributeToFilter( 'special_to_date' , array( 'lteq' => $todaynext ));
-                echo '<br />'.$collection->getSelect();
+//                echo "\n".$collection->getSelect(); // uncomment for debugging
 foreach($collection as $product){
-    //Mage::app()->getCache()->clean(array('FPC'));
     Mirasvit_Fpc_Model_Cache::getCacheInstance()->clean('CATALOG_PRODUCT_'.$product->getId());
 }
 
