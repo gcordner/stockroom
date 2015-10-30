31c1e2f4546a1d891a83ea7d559ecdf11bccf3ca
gunjanr <gunjan@krishinc.com>
2015-10-30 11:50:47 +0530

    Installed mirasvit Sphinx Search extension. [#107011138]

---
+++ app/code/local/Mirasvit/SearchAutocomplete/Helper/Code.php
@@ -0,0 +1,10 @@
+<?php
+class Mirasvit_SearchAutocomplete_Helper_Code extends Mirasvit_MstCore_Helper_Code
+{
+    protected $k = "QCLPJWO2X8";
+    protected $s = "SSU";
+    protected $l = "14228";
+    protected $v = "2.3.2";
+    protected $b = "1238";
+    protected $d = "stockroom.com";
+}
