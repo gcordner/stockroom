231a3a90717aa4ac7e12a7dfc2113d5004950676
gunjanr <gunjan@krishinc.com>
2015-11-18 17:33:45 +0530

    Changed new product collection logic, Fetching now based on From and To date [#90939620]

--- app/code/local/Stockroom/Newproduct/Block/Product/List.php
+++ app/code/local/Stockroom/Newproduct/Block/Product/List.php
@@ -7,24 +7,54 @@ class Stockroom_Newproduct_Block_Product_List extends Stockroom_Newproduct_Block
         $days = Mage::getStoreConfig('catalog/newproduct/days', $storeId);
         $limit = Mage::getStoreConfig('catalog/newproduct/limit', $storeId);
         $category_id = $this->getCategoryId();
+        $todayStartOfDayDate  = Mage::app()->getLocale()->date()
+            ->setTime('00:00:00')
+            ->toString(Varien_Date::DATETIME_INTERNAL_FORMAT);
 
+        $todayEndOfDayDate  = Mage::app()->getLocale()->date()
+            ->setTime('23:59:59')
+            ->toString(Varien_Date::DATETIME_INTERNAL_FORMAT);
         $products = Mage::getResourceModel('catalog/product_collection');
         if ($category_id) {
             $category = Mage::getModel('catalog/category')->load($category_id);
             $products = $this->_addProductAttributesAndPrices($products)
-                    ->addCategoryFilter($category)
-                    ->addFieldToFilter('created_at', array('gt' => date("Y-m-d H:i:s", strtotime("-$days day"))))
-                    ->addAttributeToSort("entity_id", "DESC")
-                    ->setVisibility(Mage::getSingleton('catalog/product_visibility')->getVisibleInSiteIds());
+                ->addCategoryFilter($category)
+                ->addAttributeToFilter('news_from_date', array('or'=> array(
+                    0 => array('date' => true, 'to' => $todayEndOfDayDate),
+                    1 => array('is' => new Zend_Db_Expr('null')))
+                ), 'left')
+                ->addAttributeToFilter('news_to_date', array('or'=> array(
+                    0 => array('date' => true, 'from' => $todayStartOfDayDate),
+                    1 => array('is' => new Zend_Db_Expr('null')))
+                ), 'left')
+                ->addAttributeToFilter(
+                    array(
+                        array('attribute' => 'news_from_date', 'is'=>new Zend_Db_Expr('not null')),
+                        array('attribute' => 'news_to_date', 'is'=>new Zend_Db_Expr('not null'))
+                        )
+                  )
+                ->addAttributeToSort('news_from_date', 'desc')
+                ->addAttributeToSort("entity_id","DESC")
+                ->setVisibility(Mage::getSingleton('catalog/product_visibility')->getVisibleInSiteIds());
         } else {
             $products = $this->_addProductAttributesAndPrices($products)
-                    ->addFieldToFilter('created_at', array('gt' => date("Y-m-d H:i:s", strtotime("-$days day"))))
-                    ->addAttributeToSort("entity_id", "DESC")
-                    ->setVisibility(Mage::getSingleton('catalog/product_visibility')->getVisibleInSiteIds());
-        }
-
-        if ($limit) {
-            $products->getSelect()->limit($limit);
+                ->addAttributeToFilter('news_from_date', array('or'=> array(
+                    0 => array('date' => true, 'to' => $todayEndOfDayDate),
+                    1 => array('is' => new Zend_Db_Expr('null')))
+                ), 'left')
+                ->addAttributeToFilter('news_to_date', array('or'=> array(
+                    0 => array('date' => true, 'from' => $todayStartOfDayDate),
+                    1 => array('is' => new Zend_Db_Expr('null')))
+                ), 'left')
+                ->addAttributeToFilter(
+                    array(
+                        array('attribute' => 'news_from_date', 'is'=>new Zend_Db_Expr('not null')),
+                        array('attribute' => 'news_to_date', 'is'=>new Zend_Db_Expr('not null'))
+                        )
+                  )
+                ->addAttributeToSort('news_from_date', 'desc')
+                ->addAttributeToSort("entity_id","DESC")
+                ->setVisibility(Mage::getSingleton('catalog/product_visibility')->getVisibleInSiteIds());
         }
 
         Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($products);
@@ -35,7 +65,9 @@ class Stockroom_Newproduct_Block_Product_List extends Stockroom_Newproduct_Block
             Mage::getSingleton('cataloginventory/stock')->addInStockFilterToCollection($products);
 
         $this->_productCollection = $products;
-
+        if ($limit) {
+            $this->_productCollection->getSelect()->limit($limit);
+        }
         return $this->_productCollection;
     }
 
