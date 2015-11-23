7a3486501f6e187151040136bd93dd170931f7c8
gunjanr <gunjan@krishinc.com>
2015-11-23 15:05:12 +0530

    Upgrade script to amend attribute code from meta_keywords to meta_keyword. [#108729374]

---
+++ app/code/local/Stockroom/Createprometa/sql/catalog_productmeta_setup/mysql4-data-upgrade-0.1.0-0.1.1.php
@@ -0,0 +1,8 @@
+<?php
+    $installer = $this;
+    $installer->startSetup();
+    $installer = $this;
+    $installer->startSetup();
+    $installer->updateAttribute('catalog_product', 'meta_keywords', array('attribute_code' => 'meta_keyword'));
+    $installer->endSetup();
+?>		 
