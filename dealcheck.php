fa4a3275b96b08c1a3cdc022dd33ccf6f401a583
Intelliant01 <intelliant01@intelliant.net>
2016-02-11 14:52:40 +0000

    1. Fixed start and end time comparison for deals display logic to consider UTC time instead of server time. 2. Implemented FPC invalidation for product when deal is activated and deactivated [#113385785]

---
+++ dealcheck.php
@@ -0,0 +1,26 @@
+<?php
+
+// Change current directory to the directory of current script
+chdir(dirname(__FILE__));
+require 'app/Mage.php';
+
+$app = Mage::app();
+$curtime = strtotime(Mage::getModel('core/date')->gmtDate());
+$todaystart =  date('Y-m-d H:i:s', mktime(date('H',$curtime),date('i',$curtime),'0', date('m',$curtime),date('d',$curtime),date('Y',$curtime)));
+$todaynext =  date('Y-m-d H:i:s', mktime(date('H',$curtime),date('i',$curtime),'59', date('m',$curtime),date('d',$curtime),date('Y',$curtime)));
+print_r($todaystart . "\n");
+print_r($todaynext . "\n");
+$collection = Mage::getModel('dailydeal/dailydeal')->getStartingdeals($todaystart, $todaynext);
+foreach($collection as $deal){
+    Mirasvit_Fpc_Model_Cache::getCacheInstance()->clean('CATALOG_PRODUCT_'.$deal->getProductId());
+    print_r("\nStarting Product ID: " . $deal->getProductId() . "\n");
+}
+
+$collection = Mage::getModel('dailydeal/dailydeal')->getEndingdeals($todaystart, $todaynext);
+foreach($collection as $deal){
+    Mirasvit_Fpc_Model_Cache::getCacheInstance()->clean('CATALOG_PRODUCT_'.$deal->getProductId());
+    print_r("\nEnding Product ID: " . $deal->getProductId() . "\n");
+}
+
+?>
+
